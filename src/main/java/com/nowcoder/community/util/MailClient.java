09060608a06bd5f8c83475368615bda58b4c5fd9
kwh <kwh361@gmail.com>
2019-12-28 14:50:20 +0800

    2.1 Add send mail

---
+++ src/main/java/com/nowcoder/community/util/MailClient.java
@@ -0,0 +1,54 @@
+package com.nowcoder.community.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+/**
+ * @author kwin
+ * @create 2019-12-28 13:38
+ */
+@Component
+public class MailClient {
+
+    private static final Logger logger = LoggerFactory.getLogger(MailClient.class);
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    /**
+     * 发信人
+     */
+    @Value("${spring.mail.username}")
+    private String from;
+
+    /**
+     * 发送邮件
+     *
+     * @param to 收信人
+     * @param subject 主题
+     * @param content 内容
+     */
+    public void sendMail(String to,String subject,String content){
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            //第二个参数设置为true后可以解析html文件
+            helper.setText(content,true);
+            mailSender.send(helper.getMimeMessage());
+        } catch (MessagingException e) {
+            logger.error("发送邮件失败:" + e.getMessage());
+        }
+    }
+}
+
