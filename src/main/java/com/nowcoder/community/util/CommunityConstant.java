ffd6529e2acfb145f470709a8679a1022f776f19
kwh <kwh361@gmail.com>
2019-12-30 15:42:06 +0800

    2.3 Add register

---
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -0,0 +1,23 @@
+package com.nowcoder.community.util;
+
+/**
+ * @author kwin
+ * @create 2019-12-29 12:44
+ */
+public interface CommunityConstant {
+
+    /**
+     * 激活成功
+     */
+    int ACTIVATE_SUCCESS = 0;
+
+    /**
+     * 重复激活
+     */
+    int ACTIVATION_REPEAT = 1;
+
+    /**
+     * 激活失败
+     */
+    int ACTIVATION_FAILURE = 2;
+}
