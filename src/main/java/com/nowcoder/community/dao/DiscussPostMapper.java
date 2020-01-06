00d7f278f0ff39acf79ca5764b39fc0e1d64e510
kwh <kwh361@gmail.com>
2020-01-06 20:12:34 +0800

    3.2 Add discussPost details

--- src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -35,5 +35,12 @@ public interface DiscussPostMapper {
      * @return
      */
     int insertDiscussPost(DiscussPost discussPost);
+
+    /**
+     * 根据id查询帖子
+     * @param id
+     * @return
+     */
+    DiscussPost selectDiscussPostById(int id);
 }
 
