143a3ae652125ffef9020b58059020e687ac3569
kwh <kwh361@gmail.com>
2020-01-09 14:03:18 +0800

    3.5 Add show discussPost comments

---
+++ src/main/java/com/nowcoder/community/dao/CommentMapper.java
@@ -0,0 +1,18 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.Comment;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * @author kwin
+ * @create 2020-01-06 22:48
+ */
+@Mapper
+public interface CommentMapper {
+
+    List<Comment> selectCommentsByEntity(int entityType, int entityId, int offset, int limit);
+
+    int selectCountByEntity(int entityType,int entityId);
+}
