33cc234f15acc04e9969904183e9322b5a21349c
kwh <kwh361@gmail.com>
2020-01-15 00:27:39 +0800

    4.2 Add Redis test

---
+++ src/main/java/com/nowcoder/community/config/RedisConfig.java
@@ -0,0 +1,33 @@
+package com.nowcoder.community.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+/**
+ * @author kwin
+ * @create 2020-01-14 23:42
+ */
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String,Object> redisTemplate(RedisConnectionFactory factory){
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+
+        //设置key的序列化方式
+        template.setKeySerializer(RedisSerializer.string());
+        //设置value的序列化方式
+        template.setValueSerializer(RedisSerializer.json());
+        //设置hash的key的序列化方式
+        template.setHashKeySerializer(RedisSerializer.string());
+        //设置hash的value的序列化方式
+        template.setHashValueSerializer(RedisSerializer.json());
+
+        template.afterPropertiesSet();
+        return template;
+    }
+}
